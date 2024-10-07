f8d064c5c391c8e1780d3df740166b763d3c414b
Guillermo Sotelo <guille.sotelo.cloud@gmail.com>
2024-10-07 14:58:22 +0200

    updated-email-template

--- api/mailer/emailTemplates.js
+++ api/mailer/emailTemplates.js
@@ -30,8 +30,8 @@ const systemDown = (data) => `<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Trans
             <td>
                 <div style="font-weight: 400; line-height: 140%; text-align: left; word-wrap: break-word;line-height: 140%; color: #666666">
                     <div style="font-size: 10px"> &nbsp;</div>
-                    <p style="font-size: 20px">Hi${data.owner ? ' ' + data.owner.split(' ')[0] : ''},</p>
-                    <p style="font-size: 18px">{process.env.APP_NAME} has detected that the system ${data.name} has been consistently marked as <span style="color: red;">down</span> for over ${getDownTimeString(data.timePassedFromDown)}.</p>
+                    <p style="font-size: 20px">Hi${data.owner ? ' ' + data.owner.split(' ')[0] : ''}</p>
+                    <p style="font-size: 18px">{process.env.APP_NAME} has detected that the system <strong>${data.name}</strong> has been consistently marked as <span style="color: red;">down</span> for over ${getDownTimeString(data.timePassedFromDown)}.</p>
                     <p style="font-size: 18px;margin: 1.4rem 0 0 0;">${data.isSubscription ? 'You are receiving this message because you are subscribed to this system.' : 'You are receiving this message because your account is associated with this system. This is just an informational message, and no action is required from you.'}</p>
                     <p style="font-size: 18px;margin: 1.4rem 0 0 0;">Regards, <br/>Team Stargate.</p>
                     
@@ -44,7 +44,7 @@ const systemDown = (data) => `<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Trans
             <td width=30px>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
         </tr>
         <tr>
-            <td style="background-color:#1b365d; padding: 0;vertical-align: center;" height=60 colspan=3>
+            <td style="background-color:#1b365d; padding: 30px;vertical-align: center;" height=60 colspan=3>
                 <div style="color: lightgray; text-align: center;">
                     <a style="color:white; text-decoration: none;" href=https://down.company.com>{process.env.APP_NAME}</a>
                 </div>
@@ -74,8 +74,8 @@ const systemUp = (data) => `<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transit
             <td>
                 <div style="font-weight: 400; line-height: 140%; text-align: left; word-wrap: break-word;line-height: 140%; color: #666666">
                     <div style="font-size: 10px"> &nbsp;</div>
-                    <p style="font-size: 20px">Hi${data.owner ? ' ' + data.owner.split(' ')[0] : ''},</p>
-                    <p style="font-size: 18px">{process.env.APP_NAME} has detected that the system ${data.name} is <span style="color: green;">up</span> again.</p>
+                    <p style="font-size: 20px">Hi${data.owner ? ' ' + data.owner.split(' ')[0] : ''}</p>
+                    <p style="font-size: 18px">{process.env.APP_NAME} has detected that the system <strong>${data.name}</strong> is <span style="color: green;">up</span> again.</p>
                     <p style="font-size: 18px;margin: 1.4rem 0 0 0;">${data.isSubscription ? 'You are receiving this message because you are subscribed to this system.' : 'You are receiving this message because your account is associated with this system. This is just an informational message, and no action is required from you.'}</p>
                     <p style="font-size: 18px;margin: 1.4rem 0 0 0;">Regards, <br/>Team Stargate.</p>
                     
@@ -88,7 +88,7 @@ const systemUp = (data) => `<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transit
             <td width=30px>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
         </tr>
         <tr>
-            <td style="background-color:#1b365d; padding: 0;vertical-align: center;" height=60 colspan=3>
+            <td style="background-color:#1b365d; padding: 30px;vertical-align: center;" height=60 colspan=3>
                 <div style="color: lightgray; text-align: center;">
                     <a style="color:white; text-decoration: none;" href=https://down.company.com>{process.env.APP_NAME}</a>
                 </div>
